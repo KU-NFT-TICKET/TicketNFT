9ee6964077ad55b0b5b74390334103e7b6aaecec
Natsatika <Natsatika.P@ku.th>
2023-04-08 18:09:52 +0700

    add global state and Profile page

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
+import accountSlicer from '../features/account/accountSlicer'
+import eventSlicer from '../features/events/eventSlicer'
 
 export default configureStore({
   reducer: {
-  	counter: counterReducer
+  	counter: counterReducer,
+  	account: accountSlicer,
+  	events: eventSlicer
   }
 })
